d0b7508cb8835f064e5d0d0061b8d73bb984811d
Alexander Kolberg <alexander.kolberg@gmail.com>
2023-12-27 23:27:49 +0100

    Basic script

---
+++ script.sh
@@ -0,0 +1,14 @@
+// Add dev folders
+mkdir ~/dev
+mkdir ~/dev/work
+mkdir ~/dev/personal
+
+// Screenshot folder, 
+mkdir ~/screenshots
+
+// Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+// Install mackup and dropbox
+brew install mackup
+brew install --cask dropbox
