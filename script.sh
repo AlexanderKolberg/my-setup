af9e18fb770c2be4569637ca17976fcdbd1ac99c
Alexander Kolberg <74478295+kolberg-dev@users.noreply.github.com>
2023-12-28 00:06:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,23 @@
-#Add dev folders
+# Add dev folders
 mkdir ~/dev
 mkdir ~/dev/work
 mkdir ~/dev/personal
 
-#Screenshot folder
+# Screenshot folder
 mkdir ~/screenshots
 
-#Install Homebrew
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#Install mackup and dropbox
+# Install oh-my-zsh, might need git first..
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install mackup and dropbox
 brew install mackup
 brew install --cask dropbox
 
-#TODO: login and wait for dropbox, before mackup restor
+# TODO: login and wait for dropbox, before mackup restore,
+
+# cp com.apple.dock.plist ~/alex/Library/Preferences/
+# killall Dock
+# cp 
