ab2e372b2a337ad6d5f385eb2656fe8e822ed0b5
Alexander Kolberg <74478295+kolberg-dev@users.noreply.github.com>
2023-12-27 23:51:06 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
-// Add dev folders
+#Add dev folders
 mkdir ~/dev
 mkdir ~/dev/work
 mkdir ~/dev/personal
 
-// Screenshot folder, 
+#Screenshot folder
 mkdir ~/screenshots
 
-// Install Homebrew
+#Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-// Install mackup and dropbox
+#Install mackup and dropbox
 brew install mackup
 brew install --cask dropbox
+
+#TODO: login and wait for dropbox, before mackup restor
